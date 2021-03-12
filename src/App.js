8980b7cf6b3a806522fb44a8fa676ce857decbf0
Serhii Stanislav <serhiistanislav@gmail.com>
2021-03-13 01:06:16 +0200

    adds axios query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { Component } from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import HomePage from './views/HomePage';
 import MoviesPage from './views/MoviesPage';
+import NotFoundPage from './views/NotFoundPage';
 
-// e548173527b69af98deb3da87ab1364c
-// https://api.themoviedb.org/3/movie/550?api_key=e548173527b69af98deb3da87ab1364c
-// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTQ4MTczNTI3YjY5YWY5OGRlYjNkYTg3YWIxMzY0YyIsInN1YiI6IjYwNGI4OThkOGEwZTliMDA3OWExY2U0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q4y4J0v5i2stJnCmjr_D4HnrTBnU3PxXq20VC9KeLFE
+axios.defaults.baseURL = 'https://api.themoviedb.org';
+const apiKey = 'e548173527b69af98deb3da87ab1364c';
 
-const App = () => (
-  <>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/movies">Movies</Link>
-      </li>
-    </ul>
-    <Route exact path="/" component={HomePage} />
-    <Route path="/movies" component={MoviesPage} />
-  </>
-);
+class App extends Component {
+  state = {
+    trends: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        `/3/trending/all/day?api_key=${apiKey}&page=1`,
+      );
+      const trends = response.data.results;
+      this.setState({ trends: trends });
+      console.log(trends[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <ul>
+          <li>
+            <NavLink
+              exact
+              to="/"
+              className="NavLink"
+              activeClassName="NavLink--active"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/movies"
+              className="NavLink"
+              activeClassName="NavLink--active"
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/movies" component={MoviesPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+
+        <ul>
+          {this.state.trends.map(
+            trend =>
+              trend.title !== undefined && (
+                <li key={trend.id}>{trend.title}</li>
+              ),
+          )}
+        </ul>
+      </>
+    );
+  }
+}
 
 export default App;
