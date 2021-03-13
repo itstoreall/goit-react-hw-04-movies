31e573da6b75c5e791ac42126e94fcee51bcac1c
Serhii Stanislav <serhiistanislav@gmail.com>
2021-03-13 22:50:19 +0200

    fixes error 404

---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Cast extends Component {
+  render() {
+    return <p>Hi</p>;
+  }
+}
--- src/views/MovieDetailsPage.jsx
+++ src/views/MovieDetailsPage.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+import Cast from '../components/Cast';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org';
 const apiKey = 'e548173527b69af98deb3da87ab1364c';
@@ -14,37 +15,41 @@ export default class MovieDetailsPage extends Component {
   };
 
   async componentDidMount() {
-    const { movieId } = this.props.match.params;
-    const response = await axios.get(`/3/movie/${movieId}?api_key=${apiKey}`);
-    console.log(response.data);
-    this.setState({ ...response.data });
+    try {
+      const { movieId } = this.props.match.params;
+      const response = await axios.get(`/3/movie/${movieId}?api_key=${apiKey}`);
+      this.setState({ ...response.data });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render() {
     const { poster_path, title, release_date, overview, genres } = this.state;
-    // const URL_POSTER = 'https://image.tmdb.org/t/p/w500';
-
-    // const KEY_POSTER = poster_path;
-    // console.log(URL_POSTER);
-    // console.log(KEY_POSTER);
-
-    const imgUrl = 'http://image.tmdb.org/t/p/w500' + poster_path;
 
     return (
       <>
-        <h1>Movie Details Page - {this.props.match.params.movieId}</h1>
-        <div>
-          <img src={imgUrl} alt={this.state.title} />
-          <h2>
-            {title} ({release_date.slice(0, 4)})
-          </h2>
-          <h3>Overview</h3>
-          <p>{overview}</p>
-          <h3>Genres</h3>
-          {genres.map(genre => (
-            <span key={genre.name}>{genre.name} </span>
-          ))}
-        </div>
+        {poster_path && (
+          <div>
+            <h1>Movie Details Page - {this.props.match.params.movieId}</h1>
+            <div>
+              <img
+                src={`https://image.tmdb.org/t/p/w300${poster_path}`}
+                alt={this.state.title}
+              />
+              <h2>
+                {title} ({release_date.slice(0, 4)})
+              </h2>
+              <h3>Overview</h3>
+              <p>{overview}</p>
+              <h3>Genres</h3>
+              {genres.map(genre => (
+                <span key={genre.name}>{genre.name} </span>
+              ))}
+            </div>
+            <Cast />
+          </div>
+        )}
       </>
     );
   }
